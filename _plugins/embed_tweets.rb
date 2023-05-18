0fa857747d9158c5a3d61d0c1ddb0c216dd1f63e
micahchoo <47268348+micahchoo@users.noreply.github.com>
2023-05-17 17:52:18 -0700

    Update embed_tweets.rb

--- _plugins/embed_tweets.rb
+++ _plugins/embed_tweets.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
-class TweetEmbedGenerator < Jekyll::Generator
+class EmbedGenerator < Jekyll::Generator
   def generate(site)
-    return if !site.config["embed_tweets"]
-
     all_notes = site.collections['notes'].docs
     all_pages = site.pages
     all_docs = all_notes + all_pages
 
     all_docs.each do |current_note|
+      # Embed tweets
       current_note.content.gsub!(
         /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)$/i,
         <<~HTML
@@ -17,6 +16,19 @@ class TweetEmbedGenerator < Jekyll::Generator
           <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         HTML
       )
+
+      # Embed YouTube videos
+      current_note.content.gsub!(
+        /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:v\/|(?:embed\/)?(?:watch\?v=)?|.*\/)|(?:youtu\.be\/))([^?\&\#]+)/,
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+      )
+
+      # Embed Vimeo videos
+      current_note.content.gsub!(
+        /(?:http?s?:\/\/)?(?:www\.)?(?:vimeo\.com\/)([0-9]+)/,
+        '<iframe src="https://player.vimeo.com/video/\1" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+      )
+
     end
   end
 end
