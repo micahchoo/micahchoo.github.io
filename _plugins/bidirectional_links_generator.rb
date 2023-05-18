92035d605e195bd1b351fe8dec858a91dba18a7e
micahchoo <47268348+micahchoo@users.noreply.github.com>
2023-05-17 17:19:48 -0700

    experiment

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -28,33 +28,33 @@ class BidirectionalLinksGenerator < Jekyll::Generator
         end
 
         new_href = "#{site.baseurl}#{note_potentially_linked_to.url}#{link_extension}"
-        anchor_tag = "<a class='internal-link' href='#{new_href}'>\\1</a>"
+        anchor_tag = "<a class='internal-link' href='#{new_href}\\2'>\\1</a>"
 
         # Replace double-bracketed links with label using note title
         # [[A note about cats|this is a link to the note about cats]]
         current_note.content.gsub!(
-          /\[\[#{note_title_regexp_pattern}\|(.+?)(?=\])\]\]/i,
+          /\[\[#{note_title_regexp_pattern}(#.*?)?\|(.+?)(?=\])\]\]/i, # Added (#.*?)? to match optional #section
           anchor_tag
         )
 
         # Replace double-bracketed links with label using note filename
         # [[cats|this is a link to the note about cats]]
         current_note.content.gsub!(
-          /\[\[#{title_from_data}\|(.+?)(?=\])\]\]/i,
+          /\[\[#{title_from_data}(#.*?)?\|(.+?)(?=\])\]\]/i,
           anchor_tag
         )
 
         # Replace double-bracketed links using note title
         # [[a note about cats]]
         current_note.content.gsub!(
-          /\[\[(#{title_from_data})\]\]/i,
+          /\[\[(#{title_from_data}(#.*?)?)\]\]/i,
           anchor_tag
         )
 
         # Replace double-bracketed links using note filename
         # [[cats]]
         current_note.content.gsub!(
-          /\[\[(#{note_title_regexp_pattern})\]\]/i,
+          /\[\[(#{note_title_regexp_pattern})(#.*?)?\]\]/i,
           anchor_tag
         )
       end
